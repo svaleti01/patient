34a92ebcd6badbb654fdd03546f6044bfb038fbf
539617 <539617@PC339073.cts.com>
2020-08-11 22:55:19 +0530

    added junit

--- src/main/java/com/patientcare/patient/model/ClaimsRequest.java
+++ src/main/java/com/patientcare/patient/model/ClaimsRequest.java
@@ -11,25 +11,18 @@ public class ClaimsRequest {
 	public String getUsername() {
 		return username;
 	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public Date getDate() {
-		return date;
-	}
-	public void setDate(Date date) {
-		this.date = date;
-	}
 	public String getDrug() {
 		return drug;
-	}
-	public void setDrug(String drug) {
-		this.drug = drug;
 	}
 	public Integer getCount() {
 		return count;
 	}
-	public void setCount(Integer count) {
+
+	public ClaimsRequest(String username, Date date, String drug, Integer count) {
+		super();
+		this.username = username;
+		this.date = date;
+		this.drug = drug;
 		this.count = count;
 	}
 
--- src/test/java/com/patientcare/patient/PatientApplicationTests.java
+++ src/test/java/com/patientcare/patient/PatientApplicationTests.java
@@ -1,13 +1,42 @@
 package com.patientcare.patient;
 
+import java.util.Calendar;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.patientcare.patient.controller.UserController;
+import com.patientcare.patient.model.ClaimsRequest;
 
 @SpringBootTest
 class PatientApplicationTests {
+	
+	@Autowired
+	UserController userController;
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	public void getAllUsersTest() {
+		userController.getAllUsers("user1");
+	}
+	
+	@Test
+	public void claimsRequestTest() {
+		// create a java calendar instance
+		Calendar calendar = Calendar.getInstance();
+
+		// get a java date (java.util.Date) from the Calendar instance.
+		// this java date will represent the current date, or "now".
+		java.util.Date currentDate = calendar.getTime();
+
+		// now, create a java.sql.Date from the java.util.Date
+		java.sql.Date date = new java.sql.Date(currentDate.getTime());
+		ClaimsRequest claimsRequest = new ClaimsRequest("user1", date, "anacin", 10);
+		userController.claimsRequest(claimsRequest);
 	}
 
 }
